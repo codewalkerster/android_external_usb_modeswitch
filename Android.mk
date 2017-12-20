61288cec5f63186f73cc0e3f2c3948dbd2693214
Alex Zhao <zzc@rock-chips.com>
2017-12-20 15:08:43 +0800

    modify usb_modeswitch for android8.x
    Change-Id: Idb584bfcb24e4223bb56516de44f2d6f16323271
    Signed-off-by: Alex Zhao <zzc@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -5,14 +5,15 @@ INCLUDES = $(LOCAL_PATH)
 INCLUDES += external/libusb/libusb
 INCLUDES += external/libusb-compat/libusb
 
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE := usb_modeswitch
 LOCAL_SRC_FILES := usb_modeswitch.c
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES :=$(INCLUDES)
 LOCAL_SHARED_LIBRARIES := libusb libusb-compat
 
-$(shell mkdir -p $(PRODUCT_OUT)/system/etc)
-$(shell cp -R $(LOCAL_PATH)/usb_modeswitch.d $(PRODUCT_OUT)/system/etc)
+$(shell mkdir -p $(PRODUCT_OUT)/vendor/etc)
+$(shell cp -R $(LOCAL_PATH)/usb_modeswitch.d $(PRODUCT_OUT)/vendor/etc)
 
 include $(BUILD_EXECUTABLE)
 
