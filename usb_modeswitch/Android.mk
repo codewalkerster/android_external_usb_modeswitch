9857c093db449e24a1b4ce89557bfe53bb0785d6
Alex Zhao <zzc@rock-chips.com>
2019-01-29 14:10:54 +0800

    add usb_dongle for Android9.x
    Change-Id: I9a3442219359a2496b3c463e115ebffe990ffba4
    Signed-off-by: Alex Zhao <zzc@rock-chips.com>

--- usb_modeswitch/Android.mk
+++ usb_modeswitch/Android.mk
@@ -3,14 +3,15 @@ include $(CLEAR_VARS)
 
 INCLUDES = $(LOCAL_PATH)
 INCLUDES += external/libusb/libusb
-INCLUDES += external/libusb-compat/libusb
 
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE := usb_modeswitch
 LOCAL_SRC_FILES := usb_modeswitch.c
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES :=$(INCLUDES)
-LOCAL_SHARED_LIBRARIES := libusb libusb-compat
+LOCAL_SHARED_LIBRARIES := libusb
+
+LOCAL_CFLAGS += -Wno-self-assign -Wno-sometimes-uninitialized
 
 $(shell mkdir -p $(PRODUCT_OUT)/vendor/etc)
 $(shell cp -R $(LOCAL_PATH)/usb_modeswitch.d $(PRODUCT_OUT)/vendor/etc)
